5ee26ebb9e5073e09a28405530d1bbb1772f33ef
Parth Sinha <sinhaparth555@gmail.com>
2024-12-15 16:01:11 +0000

    added pixel code

---
+++ 3d_rotating_cube/README.md
@@ -0,0 +1,6 @@
+# 3D Rotating Cube
+
+### Run the code
+```Bash
+g++ main.cpp -lSLD2 -std=c++20 -o pixel
+```
---
+++ 3d_rotating_cube/main.cpp
@@ -0,0 +1,14 @@
+#include "screen.h"
+
+int main() {
+    Screen screen;
+
+    for (int i = 0; i < 100; i++) {
+        screen.pixel(rand()%640, rand()%480);
+    }
+
+    while(true) {
+        screen.show();
+        screen.input();
+    }
+}
